72c0e0d62d141d210efaa689f75fb10287e56123
Marcos Mercado <marcoscoca0905@gmail.com>
2020-10-07 01:03:55 -0400

    User rooms

--- hoc/belongsRoom.js
+++ hoc/belongsRoom.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+
+import Loader from 'components/Loader'
 import { getUserRooms } from 'services/chat'
 
 // eslint-disable-next-line react/display-name
@@ -12,7 +14,17 @@ const belongsRoom = (Room) => ({ user, roomId }) => {
     })
   }, [])
 
-  return <Room roomId={roomId} user={user} />
+  return <>
+    { belongs === undefined
+      ? <Loader/>
+      : belongs
+        ? <Room roomId={roomId} user={user} />
+        : <div>
+          <h3>You don&apos;t belong to this room</h3>
+        </div>
+
+    }
+  </>
 }
 
 export default belongsRoom
