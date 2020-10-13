9047c1fb1da5447a44baf88d58da53426b513725
Marcos Mercado <marcoscoca0905@gmail.com>
2020-10-12 20:37:49 -0400

    useNearScreen

---
+++ hooks/useNearScreen.js
@@ -0,0 +1,27 @@
+import { useEffect, useState, useRef } from 'react'
+
+function useNearScreen ({ distance = '30px' }) {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        const { isIntersecting } = entries[0]
+
+        if (isIntersecting) {
+          setShow(true)
+        } else {
+          setShow(false)
+        }
+      },
+      { rootMargin: distance }
+    )
+    element.current && observer.observe(element.current)
+    return () => observer && observer.disconnect()
+  }, [element])
+
+  return [show, element]
+}
+
+export default useNearScreen
