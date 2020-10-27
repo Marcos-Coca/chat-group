b5c168810fe426b688f7c8196edb0664be946f9c
Marcos Mercado <marcoscoca0905@gmail.com>
2020-10-27 17:40:09 -0400

    useInput test

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function useInput (initialValue) {
+function useInput (initialValue = '') {
   const [value, setValue] = useState(initialValue)
 
   const onChange = (e, value) => {
